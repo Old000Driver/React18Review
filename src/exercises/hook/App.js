4a78e3f86d69a9b7c800fa40dd070e5372799d3b
GJA <792002710@qq.com>
2024-11-25 16:14:38 +0800

    hook

---
+++ src/exercises/hook/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function useToggle() {
+  const [value, setValue] = useState(true);
+  const toggle = () => setValue(!value);
+  return {
+    value,
+    toggle,
+  };
+}
+function App() {
+  const { value, toggle } = useToggle();
+  return (
+    <div className="App">
+      {value && <div>this is div</div>}
+      <button onClick={toggle}>toggle</button>
+    </div>
+  );
+}
+
+export default App;
