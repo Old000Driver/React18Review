3e79cad6477be0c732428743c3119b7ca3530dee
GJA <792002710@qq.com>
2024-11-27 10:19:02 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { createRoot } from "react-dom/client";
+import store from "./store/index.js";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
 // import App from './src/exercises/bilibili_comment/App'
 
 // const root = createRoot(document.querySelector('#root'))
@@ -9,5 +12,10 @@ const exercise = process.env.REACT_APP_EXERCISE || "bilibili_comment";
 
 import(`./exercises/${exercise}/App.js`).then(({ default: App }) => {
   const root = createRoot(document.querySelector("#root"));
-  root.render(<App />);
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
+reportWebVitals();
