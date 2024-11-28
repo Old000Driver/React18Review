b73a0b0c35964fe2c5477387f383c223b986d805
GJA <792002710@qq.com>
2024-11-28 17:12:05 +0800

    meituan_End

--- src/exercises/meituan/components/Cart/index.js
+++ src/exercises/meituan/components/Cart/index.js
@@ -1,21 +1,42 @@
 import classNames from "classnames";
 import Count from "../Count";
 import "./index.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  incrementCount,
+  decrementCount,
+  clearCart,
+} from "../../../../store/modules/takeaway";
+import { useState } from "react";
 
 const Cart = () => {
   const { cartList } = useSelector((state) => state.foods);
   const totalPrice = cartList.reduce((a, c) => a + c.price * c.count, 0);
+  const dispatch = useDispatch();
 
   const cart = [];
+  const [visible, setVisible] = useState(false);
+  const onShow = () => {
+    if (cartList.length > 0) setVisible(true);
+  };
+  const onClearCart = () => {
+    setVisible(false);
+    dispatch(clearCart());
+  };
   return (
     <div className="cartContainer">
       {/* 遮罩层 添加visible类名可以显示出来 */}
-      <div className={classNames("cartOverlay")} />
+      <div
+        onClick={() => setVisible(false)}
+        className={classNames("cartOverlay", visible && "visible")}
+      />
       <div className="cart">
         {/* fill 添加fill类名可以切换购物车状态*/}
         {/* 购物车数量 */}
-        <div className={classNames("icon", cartList.length > 0 && "fill")}>
+        <div
+          onClick={onShow}
+          className={classNames("icon", cartList.length > 0 && "fill")}
+        >
           {cartList.length > 0 && (
             <div className="cartCornerMark">{cartList.length}</div>
           )}
@@ -38,15 +59,17 @@ const Cart = () => {
         )}
       </div>
       {/* 添加visible类名 div会显示出来 */}
-      <div className={classNames("cartPanel")}>
+      <div className={classNames("cartPanel", visible && "visible")}>
         <div className="header">
           <span className="text">购物车</span>
-          <span className="clearCart">清空购物车</span>
+          <span className="clearCart" onClick={onClearCart}>
+            清空购物车
+          </span>
         </div>
 
         {/* 购物车列表 */}
         <div className="scrollArea">
-          {cart.map((item) => {
+          {cartList.map((item) => {
             return (
               <div className="cartItem" key={item.id}>
                 <img className="shopPic" src={item.picture} alt="" />
@@ -60,7 +83,12 @@ const Cart = () => {
                   </div>
                 </div>
                 <div className="skuBtnWrapper btnGroup">
-                  <Count count={item.count} />
+                  {/*  */}
+                  <Count
+                    count={item.count}
+                    onPlus={() => dispatch(incrementCount({ id: item.id }))}
+                    onMinus={() => dispatch(decrementCount({ id: item.id }))}
+                  />
                 </div>
               </div>
             );
--- src/store/modules/takeaway.js
+++ src/store/modules/takeaway.js
@@ -15,16 +15,35 @@ const foodsStore = createSlice({
       state.activeIndex = action.payload;
     },
     addCart(state, action) {
-      const item = state.cartList.find((item) => item.id === action.payload.id  );
-      if (item){
+      const item = state.cartList.find((item) => item.id === action.payload.id);
+      if (item) {
         item.count++;
-      } 
-      else state.cartList.push(action.payload);
+      } else state.cartList.push(action.payload);
+    },
+    incrementCount(state, action) {
+      const item = state.cartList.find((item) => item.id === action.payload.id);
+      item.count++;
+    },
+
+    decrementCount(state, action) {
+      const item = state.cartList.find((item) => item.id === action.payload.id);
+      if (item.count === 0) return;
+      item.count--;
+    },
+    clearCart(state) {
+      state.cartList = [];
     },
   },
 });
 
-const { setFoodsList, changeActiveIndex, addCart } = foodsStore.actions;
+const {
+  setFoodsList,
+  changeActiveIndex,
+  addCart,
+  incrementCount,
+  decrementCount,
+  clearCart
+} = foodsStore.actions;
 const fetchFoodsList = () => {
   return async (dispatch) => {
     const res = await axios.get("http://localhost:3004/takeaway");
@@ -32,7 +51,14 @@ const fetchFoodsList = () => {
   };
 };
 
-export { fetchFoodsList, changeActiveIndex, addCart };
+export {
+  fetchFoodsList,
+  changeActiveIndex,
+  addCart,
+  incrementCount,
+  decrementCount,
+  clearCart
+};
 
 const reducers = foodsStore.reducer;
 export default reducers;
